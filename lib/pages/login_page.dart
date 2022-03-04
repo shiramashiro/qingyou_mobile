e9cce6e794e2537b7ff92e19ca30a3dc7e8a81cc
shiramashiro <zhengrenfu@outlook.com>
2022-03-04 18:15:08 +0800

    完善账号输入验证逻辑

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -40,54 +40,81 @@ class _LoginPageState extends State<LoginPage> {
   TextEditingController accCtrl = TextEditingController();
   TextEditingController pwdCtrl = TextEditingController();
 
-  late bool isUname;
-  late bool isEmail;
-  late bool isPhone;
+  bool isUname = false;
+  bool isEmail = false;
+  bool isPhone = false;
 
   /// @desc: 验证账号格式是否正确
   /// @author: shiramashiro
   /// @date: 2022/3/3
-  String? verifyAccount(String? validator) {
-    if (validator!.isEmpty) {
-      return "账号不能为空";
-    }
+  String? verifyAccount(String? e) {
+    /// 如果账号未输入，下面的步骤全部终止进行。
+    if (e!.isEmpty) return "账号不能为空";
 
-    isPhone = RegExp(r"^1(3\d|4[5-9]|5[0-35-9]|6[567]|7[0-8]|8\d|9[0-35-9])\d{8}$")
-        .hasMatch(validator);
-    isUname = !(isPhone && RegExp(r"^[a-zA-Z0-9_-]{4,16}$").hasMatch(validator));
-    bool isUname2Email = false;
-    isEmail = false;
+    /// 声明手机号、用户名以及电子邮箱的正则表达式。
+    String phoneRegExp = r"^1(3\d|4[5-9]|5[0-35-9]|6[567]|7[0-8]|8\d|9[0-35-9])\d{8}$";
+    String unameRegExp = r"^[a-zA-Z0-9_-]{4,16}$";
+    String emailRegExp =
+        r"^[a-z]([a-z0-9]*[-_\.]?[a-z0-9]+)*@([a-z0-9]*[-_]?[a-z0-9]+)+[\.][a-z]{2,3}([\.][a-z]{2})?$";
+    String uname2PhoneRegExp = r"^1(3\d|4[5-9]|5[0-35-9]|6[567]|7[0-8]|8\d|9[0-35-9])";
 
+    /// 用户输入的账号是否符合手机号格式。
+    isPhone = RegExp(phoneRegExp).hasMatch(e);
+
+    isEmail = RegExp(emailRegExp).hasMatch(e);
+
+    /// 用户名的正则表达式中包含了数字，所以通常情况下，用户名与手机号的正则都匹配。
+    /// 对于这类情况，解决方案时：[isPhone] 和 [isUname] 进行且运算，最终结果取反。
+    ///
+    /// 假设场景：
+    /// 手机号为 true，用户名也为 true（开头解释了原因），无法分辨用户输入的是手机号还是用户名。
+    /// 假设，用户想通过手机号方式登录，[isUname] 应该为 false。
+    ///
+    /// 且运算原理：
+    /// 如果第一个操作对象为 false，运算第二个操作对象，如果两个操作对象都为 false ，结果返回 false。
+    /// 如果第一个操作对象为 true，运算第二个操作对象，如果操作对象为 false，结果返回 false；如果操作对象 true，结果返回 true。
+    ///
+    /// 综上所述：
+    /// 手机号为 false，用户名为 true，结果返回 false。说明用户正在以用户名的方式进行输入，因此再取反，最终为 true。
+    /// 手机号为 true，用户名为 true，结果返回 true，说明用户正在以手机号的方式进行输入，因此再取反，最终为 false。
+    isUname = !(isPhone && RegExp(unameRegExp).hasMatch(e));
+
+    // 当匹配到用户名时，需要做以下几点操作，确保是否为用户名
     if (isUname) {
-      if (validator.characters.length < 6) {
-        return "用户名不能少于6位字符";
-      }
-      isUname2Email = RegExp(r"@").hasMatch(validator);
-      if (isUname2Email) {
-        isUname = false;
-      }
-    }
-
-    if (isUname2Email) {
-      bool editingIsEmail = RegExp(
-              r"^[a-z]([a-z0-9]*[-_\.]?[a-z0-9]+)*@([a-z0-9]*[-_]?[a-z0-9]+)+[\.][a-z]{2,3}([\.][a-z]{2})?$")
-          .hasMatch(validator);
-      if (editingIsEmail && isUname2Email) {
-        isEmail = true;
-      }
-    }
-
-    if (isEmail || isUname || isPhone) {
-      return null;
-    } else {
-      if (isEmail == false) {
-        return "你输入的邮箱格式有问题";
-      } else if (isUname == false) {
-        return "你输入的用户名格式有误";
-      } else if (isPhone == false) {
-        return "你输入的电话格式有误";
+      // 如果用户名有手机号开头几位，例如 185，就说明用户输入的手机号
+      if (RegExp(uname2PhoneRegExp).hasMatch(e)) {
+        if (!isPhone) {
+          // 但是最开始作了一个手机号验证，没有输入完整时，会提示电话个事有误
+          return "你输入的电话格式有误";
+        } else {
+          // 如果输入的电话正确，且包含了手机号前3位数时不提示错误
+          return null;
+        }
+        // 如果用户名没有手机号开头的几位，就说明是除了手机号外的两种类型
       } else {
+        // 判断是否包含@符号，如果是就说明正在以邮箱输入，但是没有输入完整
+        if (RegExp(r"@").hasMatch(e)) {
+          // 没有输入完整，就说明并不匹配邮箱格式
+          if (!isEmail) {
+            return "你输入的邮箱格式有误";
+            // 如果匹配成功，就不提示错误
+          } else {
+            return null;
+          }
+        }
+
+        // 如果不包含@符号，就说明是一个用户名输入，并且判断是否大于或等于6位字符
+        if (e.characters.length < 6) {
+          return "用户名不能少于6位字符";
+        } else {
+          return null;
+        }
+      }
+    } else {
+      if (isPhone) {
         return null;
+      } else {
+        return "你输入的用户名格式有误";
       }
     }
   }
@@ -218,8 +245,11 @@ class _LoginPageState extends State<LoginPage> {
                       } else {
                         user = User(psw: pwdCtrl.text, phone: accCtrl.text);
                       }
-                      Future res =
-                          Dio().post('http://10.0.2.2:8080/users/login', data: user);
+                      Dio dio = Dio(BaseOptions());
+                      Future res = dio.post(
+                        'http://localhost:8080/users/login',
+                        data: user,
+                      );
                       EasyLoading.instance
                         ..radius = 20
                         ..maskType = EasyLoadingMaskType.clear
@@ -235,7 +265,15 @@ class _LoginPageState extends State<LoginPage> {
                         } else if (data['state'] == 5002) {
                           EasyLoading.showToast('密码错误');
                         }
-                      });
+                      }, onError: (e) {
+                        EasyLoading.dismiss();
+                      }).timeout(
+                        const Duration(milliseconds: 15000),
+                        onTimeout: () {
+                          EasyLoading.dismiss();
+                          EasyLoading.showToast('请求超时');
+                        },
+                      );
                     }
                   },
                   padding: const EdgeInsets.all(20),
