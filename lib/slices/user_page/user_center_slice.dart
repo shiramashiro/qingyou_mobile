6758f33591b4fcff76e6e61ddaf4a64ec06df0b2
shiramashiro <zhengrenfu@outlook.com>
2022-03-10 21:45:02 +0800

    多功能列表组件V1.2，可以随意创建一层或者多层的Widget组件

--- lib/slices/user_page/user_center_slice.dart
+++ lib/slices/user_page/user_center_slice.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:qingyuo_mobile/components/actionable_list.dart';
+import 'package:qingyuo_mobile/components/avatar.dart';
 
 class UserCenterSlice extends StatefulWidget {
   const UserCenterSlice({Key? key}) : super(key: key);
@@ -13,7 +14,8 @@ class _UserCenterSliceState extends State<UserCenterSlice> {
     'uname': '椎名白白',
     'sex': '男',
     'signature': 'Time tick away, dream faded away!',
-    'uid': '7021686'
+    'uid': '7021686',
+    'avatar': 'assets/images/95893409_p0.jpg'
   };
 
   @override
@@ -26,6 +28,12 @@ class _UserCenterSliceState extends State<UserCenterSlice> {
       body: ActionableList(
         data: data,
         template: [
+          ActionableListTemplate(
+            label: '头像',
+            field: 'avatar',
+            onTap: () {},
+            created: (e) => Avatar(url: e, size: 50),
+          ),
           ActionableListTemplate(
             label: '昵称',
             field: 'uname',
